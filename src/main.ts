44cf65345e844cfe5736302edd7ca29c5e446ad6
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-04-25 18:03:42 -0400

    feat: added logger and global pipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Boostrap');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(3000);
+  logger.log(`App running on port 3000`);
 }
 bootstrap();
